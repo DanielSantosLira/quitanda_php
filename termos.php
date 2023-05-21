6199b579542ab1c72893f18bf284049870f1536c
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-05-21 14:07:53 -0300

    Update termos.php

--- termos.php
+++ termos.php
@@ -58,7 +58,7 @@
         <div class="container">
         <h2>1. Termos</h2>
             <p>Ao acessar ao site <a href='http://www.dominio.com'>Quitanda Online</a>, concorda em cumprir estes termos
-                de serviço, todas as leis e regulamentos aplicáveis ​​e concorda que é responsável pelo cumprimento de
+                de serviço, todas as leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de
                 todas as leis locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou
                 acessar este site. Os materiais contidos neste site são protegidos pelas leis de direitos autorais e
                 marcas comerciais aplicáveis.</p>
