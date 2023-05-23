7c27a8f885b4de13812521efb1e4addc9e88e9ab
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-05-23 16:18:39 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "127.0.0.1";
+$usuario = "root";
+$senha_bd = "";
+$banco_de_dados = "db_quitanda";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha_bd, $banco_de_dados);
+?>
