55a9a799bfbc4d1576190cea75bb13a69c1973a6
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-05-23 16:18:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -25,60 +25,61 @@
 
 </head>
 
+ 
 <body style="min-width:372px;">
     
-     <nav class="navbar navbar-expand-lg navbar-dark bg-danger border-bottom shadow-sm mb-3">
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger border-bottom shadow-sm mb-3">
 
-          <div class="container">
+<div class="container">
+     
 
-               <a href="#" class="navbar-brand"><strong>Quintanda Online</strong></a>
+    <a href="#" class="navbar-brand"><strong>Quintanda Online</strong></a>
 
-               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse">
 
-                    <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
 
-               </button>
+    </button>
 
-               <div class="navbar-collapse collapse">
+    <div class="navbar-collapse collapse">
 
-                    <ul class="navbar-nav flex-grow-1">
+        <ul class="navbar-nav flex-grow-1">
 
-                         <li class="nav-item">
-                              <a href="#" class="nav-link text-white">Principal</a>
-                         </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-white">Principal</a>
+            </li>
 
-                         <li class="nav-item">
-                              <a href="contato.php" class="nav-link text-white">Contatos</a>
-                         </li>
-                    </ul>
+            <li class="nav-item">
+                <a href="contato.php" class="nav-link text-white">Contatos</a>
+            </li>
+        </ul>
 
 
-                    <div class="align-self-end">
+        <div class="align-self-end">
 
-                         <ul class="navbar-nav">
+            <ul class="navbar-nav">
 
-                              <li class="nav-item">
-                                   <a href="cadastro.php" class="nav-link text-white">Quero Me Cadastrar</a>
-                              </li>
+                <li class="nav-item">
+                    <a href="cadastro.php" class="nav-link text-white">Quero Me Cadastrar</a>
+                </li>
 
-                              <li class="nav-item">
-                                   <a href="login.php" class="nav-link text-white">Entrar</a>
-                              </li>
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link text-white">Entrar</a>
+                </li>
 
-                              <li class="nav-item">
-                                   <a href="carrinho.php" class="nav-link text-white">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-dash text-whitgh" viewBox="0 0 16 16">
-                                             <path d="M6.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z" />
-                                             <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                                        </svg>
-                                   </a>
-                              </li>
-                         </ul>
-                    </div>
-               </div>
-          </div>
-     </nav>
-    
+                <li class="nav-item">
+                    <a href="carrinho.php" class="nav-link text-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-dash text-whitgh" viewBox="0 0 16 16">
+                            <path d="M6.5 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z" />
+                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</nav>
 
      <header class="container">
           <div id="carouselMain" class="carousel carousel-white slide " data-ride="carousel">
