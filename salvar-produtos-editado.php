43c9da1a1b80bc20cba7b890be1167838e884c95
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2023-06-01 21:53:51 -0300

    salvar2

--- salvar-produtos-editado.php
+++ salvar-produtos-editado.php
@@ -13,7 +13,7 @@ include "conexao.php";
 
 $sql_iditar_produtos = "update produtos set titulo= '$titulo', categoria='$categoria', video='$video' , foto='$foto', foto1='$foto1', foto2='$foto2', valor='$valor  where id = $id";
 
-$um_jogo = mysqli_query($conexao, $sql_iditar_produtos);
+$um_produtos = mysqli_query($conexao, $sql_iditar_produtos);
 
 header("location:listar-produtos.php?msg=sucesso");
 ?>
