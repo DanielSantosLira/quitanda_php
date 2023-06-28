150619ccff4ae2e5bda7ad48d7f2124802943923
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-06-28 20:28:36 -0300

    salvar

--- cadastro-login.php
+++ cadastro-login.php
@@ -1,58 +1,53 @@
-<?php 
- include "menu.php";
+<?php
+include "menu.php";
 ?>
 
+<main>
+
+     <div class="container">
+          <div class="row justify-content-center">
+
+
+               <form action="login.php" method="post" class="col-sm-10 col-md-8 col-lg-6">
+                    <h1 class="mb-3">Indentifique-se, por favor</h1>
+                    <div class="form-floating mb-3">
+                         <input type="email" class="form-control" autofocus aria-label="E-mail" name="email" id="email">
+                         <label for="email">E-mail</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                         <input type="password" class="form-control" autofocus aria-label="Senha" name="senha" id="senha">
+                         <label for="senha">Senha</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                         <input type="password" class="form-control" autofocus aria-label="Senha" name="Conf_Senha" id="Conf_Senha">
+                         <label for="Conf-Senha">Confirme a Senha</label>
+                    </div>
+                    <div class="form-check mb-3">
+                         <input type="checkbox" class="form-check-input" id="lembrar">
+                         <label for="lembrar" class="form-check-label">Lembrar de mim</label>
+                    </div>
+
+                    <button class="btn btn-lg btn-danger me-2" type="submit">Entar</button>
 
 
 
-     <main>
+                    <p class="mt-3">
+                         Ainda não é casdastrado? <a href="novo-usuario.php">Clique aqui</a> para se
+                         cadastar
+                    </p>
 
-          <div class="container">
-               <div class="row justify-content-center">
+                    <p class="mt-3">
+                         Esqueseu sua senha?<a href="recuperar-senha.php">Clique aqui</a> para
+                         recuperé-la.
+                    </p>
+                    <?php
+                    $mesagem = $_GET["msg"] ?? "";
+                    if ($mesagem == "erro") {
+                         echo "<em class='text-danger'>Usuário ou senha inválida</em>";
+                    }
+                    ?>
+               </form>
 
-
-                    <form action="login.php" method="post" class="col-sm-10 col-md-8 col-lg-6">
-                         <h1 class="mb-3">Indentifique-se, por favor</h1>
-                         <div class="form-floating mb-3">
-                              <input type="email" class="form-control" autofocus aria-label="E-mail" name="email" id="email">
-                              <label for="email">E-mail</label>
-                         </div>
-                         <div class="form-floating mb-3">
-                              <input type="password" class="form-control" autofocus aria-label="Senha" name="senha" id="senha">
-                              <label for="senha">Senha</label>
-                         </div>
-                         <div class="form-floating mb-3">
-                              <input type="password" class="form-control" autofocus aria-label="Senha" name="Conf_Senha" id="Conf_Senha">
-                              <label for="Conf-Senha">Confirme a Senha</label>
-                         </div>
-                         <div class="form-check mb-3">
-                              <input type="checkbox" class="form-check-input" id="lembrar">
-                              <label for="lembrar" class="form-check-label">Lembrar de mim</label>
-                         </div>
-
-                         <button class="btn btn-lg btn-danger me-2" type="submit">Entar</button>
-
-
-
-                         <p class="mt-3">
-                              Ainda não é casdastrado? <a href="novo-usuario.php">Clique aqui</a> para se
-                              cadastar
-                         </p>
-
-                         <p class="mt-3">
-                              Esqueseu sua senha?<a href="recuperar-senha.php">Clique aqui</a> para
-                              recuperé-la.
-                         </p>
-                         <?php
-                         $mesagem = $_GET["msg"] ?? "";
-                         if ($mesagem == "erro") {
-                              echo "<em class='text-danger'>Usuário ou senha inválida</em>";
-                         }
-                         ?>
-                    </form>
-
-               </div>
           </div>
-     </main>
-
-
+     </div>
+</main>
--- menu.php
+++ menu.php
@@ -44,7 +44,7 @@
                     <ul class="navbar-nav flex-grow-1">
 
                          <li class="nav-item">
-                              <a href="#" class="nav-link text-white">Principal</a>
+                              <a href="index.php" class="nav-link text-white">Principal</a>
                          </li>
 
                          <li class="nav-item">
@@ -80,9 +80,7 @@
      </nav>
 
 
-    <!--  <div style="height: 273px;" class="d-block d-md-none"></div>
-     <div style="height: 153px;" class="d-none d-md-block d-lg-none"></div>
-     <div style="height: 129px;" class="d-none d-lg-block"></div> -->
+   
 
 
      <footer class="border-top fixed-bottom text-muted bg-light">
