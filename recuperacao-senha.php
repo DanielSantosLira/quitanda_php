23072d4b06f42a8e6c0d6c24f26b876288047d0d
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-06-28 22:25:12 -0300

    salvar

--- recuperacao-senha.php
+++ recuperacao-senha.php
@@ -5,7 +5,25 @@ include "menu.php";
 
 
 <main>
-     
+     <?php
+     include "conexao.php";
+
+     $email = $_POST["email"];
+     $senha = md5($_POST["senha"]);
+     $Conf_Senha = md5($_POST["Conf_Senha"]);
+
+
+     $sql_buscar_usuario = "update * from usuario where email = '$email' and senha = '$senha' and conf_Senha = '$Conf_Senha'";
+
+     $um_usuario = mysqli_query($conexao, $sql_buscar_usuario);
+
+     if ($um_usuario->num_rows > 0) {
+          header("location:painel.php");
+     } else {
+          header("location:cadastro-login.php?msg=erro");
+     }
+     mysqli_close($conexao);
+     ?>
 
      <div class="container">
           <h1>Instruções Enviadas!</h1>
